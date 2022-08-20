6a44f12e560452da94191f9a2423a605a4a414d0
Mytag <Mytag@students.softuni.bg>
2022-08-20 13:59:16 +0300

    Final update on the entities

--- Data/MHome.Data.Models/Order.cs
+++ Data/MHome.Data.Models/Order.cs
@@ -1,6 +1,9 @@
 ﻿using MHome.Data.Common.Models;
+using MHome.Data.Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,24 @@ namespace MHome.Data.Models
         public Order()
         {
             this.Id = Guid.NewGuid().ToString();
+            this.OrderedFurniture = new List<Furniture>();
+            this.OrderedAccesorries = new List<Accessory>();
         }
+
+        [Required]
+        [ForeignKey(nameof(Client))]
+        public string ClientId { get; set; }
+
+        public DateTime TimeOfOrder { get; set; }
+
+        public virtual Client Client { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public DeliveryType DeliveryType { get; set; }
+
+        public virtual ICollection<Furniture> OrderedFurniture { get; set; }
+
+        public virtual ICollection<Accessory> OrderedAccesorries { get; set; }
     }
-}
+} 
