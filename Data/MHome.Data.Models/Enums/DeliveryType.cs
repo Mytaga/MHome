4bbbc60623c0f82bebb132d2b4d8fa0eabf550d6
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-08-20 14:02:15 +0300

    Add files via upload

---
+++ Data/MHome.Data.Models/Enums/DeliveryType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHome.Data.Models.Enums
+{
+    public enum DeliveryType
+    {
+        OnSite,
+        Courier,
+        Post,
+    }
+}
