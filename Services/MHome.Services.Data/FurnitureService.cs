4e2129b8e1834a9266015e685414cb1366250213
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-08-21 22:56:09 +0300

    Add files via upload

---
+++ Services/MHome.Services.Data/FurnitureService.cs
@@ -0,0 +1,56 @@
+﻿using MHome.Data.Common.Repositories;
+using MHome.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHome.Services.Data
+{
+    public class FurnitureService : IFurnitureService
+    {
+        private const string EmptyString = "";
+        private readonly IDeletableEntityRepository<Furniture> furnitureRepo;
+
+        public FurnitureService(IDeletableEntityRepository<Furniture> furnitureRepo)
+        {
+            this.furnitureRepo = furnitureRepo;
+        }
+
+        public async Task<ICollection<Furniture>> GetAllByName(string searchName = EmptyString)
+        {
+            if (searchName != EmptyString)
+            {
+                return await this.furnitureRepo
+                    .AllAsNoTracking()
+                    .Where(f => f.Name.ToLower().Contains(searchName.ToLower()))
+                    .ToArrayAsync();
+            }
+
+            return await this.furnitureRepo.All().ToArrayAsync();
+        }
+
+        public async Task<ICollection<Furniture>> GetAllByCategory(string categoryName = EmptyString)
+        {
+            if (categoryName != EmptyString)
+            {
+                return await this.furnitureRepo
+                    .AllAsNoTracking()
+                    .Where(f => f.Category.Name.ToLower().Contains(categoryName.ToLower()))
+                    .ToArrayAsync();
+            }
+
+            return await this.furnitureRepo.All().ToArrayAsync();
+        }
+
+        public async Task<Furniture> GetById(string id)
+        {
+            return await this.furnitureRepo
+                .AllAsNoTracking()
+                .FirstOrDefaultAsync(f => f.Id == id);
+        }
+    }
+}
---
+++ Services/MHome.Services.Data/IFurnitureService.cs
@@ -0,0 +1,18 @@
+﻿using MHome.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHome.Services.Data
+{
+    public interface IFurnitureService
+    {
+        Task<ICollection<Furniture>> GetAllByName(string searchName = "");
+
+        Task<ICollection<Furniture>> GetAllByCategory(string categoryName = "");
+
+        Task<Furniture> GetById(string id);
+    }
+}
