b3895a70fa6cda80c504cae17e6c0740d6cf1b72
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-08-21 22:55:28 +0300

    Add files via upload

---
+++ Web/MHome.Web/Controllers/FurnitureController.cs
@@ -0,0 +1,25 @@
+﻿using MHome.Data.Models;
+using MHome.Services.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MHome.Web.Controllers
+{
+    public class FurnitureController : BaseController
+    {
+        private readonly IFurnitureService furnitureService;
+        public FurnitureController(IFurnitureService furnitureService)
+        {
+            this.furnitureService = furnitureService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All(string search)
+        {
+            ICollection<Furniture> allFurnitures = await this.furnitureService.GetAllByName();
+
+            return this.View(allFurnitures);
+        }
+    }
+}
