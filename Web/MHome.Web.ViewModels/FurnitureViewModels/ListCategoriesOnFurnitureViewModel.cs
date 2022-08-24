339f5e34e1d5169928b586fa458f461c155113e8
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-08-24 14:14:39 +0300

    Add files via upload

---
+++ Web/MHome.Web.ViewModels/FurnitureViewModels/ListCategoriesOnFurnitureViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MHome.Data.Models;
+using MHome.Services.Mapping;
+
+namespace MHome.Web.ViewModels.FurnitureViewModels
+{
+    public class ListCategoriesOnFurnitureViewModel : IMapFrom<Category>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
