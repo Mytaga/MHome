e08e041a96566810ebf8c4abb03dab3c2dc96a2e
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-08-27 16:58:02 +0300

    Add files via upload

---
+++ Web/MHome.Web.ViewModels/FurnitureViewModels/EditFurnitureInputModel.cs
@@ -0,0 +1,38 @@
+﻿using MHome.Data.Models;
+using MHome.Data.Models.Common;
+using MHome.Services.Mapping;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MHome.Web.ViewModels.FurnitureViewModels
+{
+    public class EditFurnitureInputModel : IMapTo<Furniture>
+    {
+        [Required(ErrorMessage = FurnitureValidationConstants.NameIsRequiredError)]
+        [MinLength(FurnitureValidationConstants.NameMinLength, ErrorMessage = FurnitureValidationConstants.NameMinLengthError)]
+        [MaxLength(FurnitureValidationConstants.NameMaxLength, ErrorMessage = FurnitureValidationConstants.NameMaxLengthError)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = FurnitureValidationConstants.DescriptionIsRequiredError)]
+        [MinLength(FurnitureValidationConstants.DescriptionMinLength, ErrorMessage = FurnitureValidationConstants.DescriptionMinLengthError)]
+        [MaxLength(FurnitureValidationConstants.DescriptionMaxLength, ErrorMessage = FurnitureValidationConstants.DescriptionMaxLengthError)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), FurnitureValidationConstants.PriceMinValue, FurnitureValidationConstants.PriceMaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ImageURL { get; set; }
+
+        [Required(ErrorMessage = FurnitureValidationConstants.DimensionsAreRequiredError)]
+        [MinLength(FurnitureValidationConstants.DimensionsMinLength, ErrorMessage = FurnitureValidationConstants.DimensionsMinLengthError)]
+        [MaxLength(FurnitureValidationConstants.DimensionsMaxLength, ErrorMessage = FurnitureValidationConstants.DimensionsMaxLengthError)]
+        public string Dimensions { get; set; }
+
+        [Range(FurnitureValidationConstants.StockQuantityMinValue, FurnitureValidationConstants.StockQuantityMaxValue)]
+        public int StockQuantity { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Web/MHome.Web.ViewModels/FurnitureViewModels/EditFurnitureViewModel.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MHome.Data.Models;
+using MHome.Services.Mapping;
+using System.Collections.Generic;
+
+namespace MHome.Web.ViewModels.FurnitureViewModels
+{
+    public class EditFurnitureViewModel : IMapFrom<Furniture>, IHaveCustomMappings
+    {
+        public EditFurnitureViewModel()
+        {
+            this.Categories = new List<ListCategoriesOnFurnitureViewModel>();
+        }
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Dimensions { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public ICollection<ListCategoriesOnFurnitureViewModel> Categories { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Category, ListCategoriesOnFurnitureViewModel>()
+                .ForMember(d => d.Id, mo => mo.MapFrom(s => s.Id))
+                .ForMember(d => d.Name, mo => mo.MapFrom(s => s.Name));
+        }
+    }
+}
