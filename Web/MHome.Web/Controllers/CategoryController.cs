4b6bc8889b557e9d29e14aa8051464a83eb6d131
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-08-29 15:42:21 +0300

    Add files via upload

---
+++ Web/MHome.Web/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using MHome.Common;
+using MHome.Data.Models;
+using MHome.Services.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace MHome.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.RedirectToAction("Create", "Category");
+            }
+
+            if (this.categoryService.ExistById(category.Id))
+            {
+                return this.RedirectToAction("Create", "Category");
+            }
+
+            await this.categoryService.AddCategory(category);
+
+            return this.RedirectToAction("Index", "Home");
+        }
+    }
+}
