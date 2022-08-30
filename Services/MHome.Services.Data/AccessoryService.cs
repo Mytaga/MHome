26f603b8f2fc7cf16997f603ab8d1cca7dafcd94
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-08-30 15:40:00 +0300

    Add files via upload

---
+++ Services/MHome.Services.Data/AccessoryService.cs
@@ -0,0 +1,61 @@
+﻿using MHome.Data.Common.Repositories;
+using MHome.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHome.Services.Data
+{
+    public class AccessoryService : IAccessoryService
+    {
+        private const string EmptyString = "";
+        private readonly IDeletableEntityRepository<Accessory> accessoryRepo;
+
+        public AccessoryService(IDeletableEntityRepository<Accessory> accessoryRepo)
+        {
+            this.accessoryRepo = accessoryRepo;
+        }
+
+        public Task AddAccessory(Accessory accessory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAccesory(Accessory accessory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAccessory(Accessory accessory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Accessory> GetAllByName(string searchName = "")
+        {
+            if (searchName != null)
+            {
+                return this.accessoryRepo
+                    .AllAsNoTracking()
+                    .Where(f => f.Name.ToLower().Contains(searchName.ToLower()));
+            }
+
+            return this.accessoryRepo.All();
+        }
+
+        public Accessory GetById(string id)
+        {
+            return this.accessoryRepo
+              .All()
+              .FirstOrDefault(f => f.Id == id);
+        }
+
+        public async Task<Accessory> GetByIdАsync(string id)
+        {
+            return await this.accessoryRepo
+               .All()
+               .FirstOrDefaultAsync(f => f.Id == id);
+        }
+    }
+}
---
+++ Services/MHome.Services.Data/IAccessoryService.cs
@@ -0,0 +1,22 @@
+﻿using MHome.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHome.Services.Data
+{
+    public interface IAccessoryService
+    {
+        IQueryable<Accessory> GetAllByName(string searchName = "");
+
+        Task<Accessory> GetByIdАsync(string id);
+
+        Accessory GetById(string id);
+
+        Task AddAccessory(Accessory accessory);
+
+        void DeleteAccesory(Accessory accessory);
+
+        Task EditAccessory(Accessory accessory);
+    }
+}
