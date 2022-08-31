acc3d7b92d997380515744e6147d352a921b301e
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-08-31 15:32:45 +0300

    Add files via upload

--- MHome.Data.Models.Common/AccessoryValidationConstants.cs
+++ MHome.Data.Models.Common/AccessoryValidationConstants.cs
@@ -8,7 +8,22 @@ namespace MHome.Data.Models.Common
 {
     public static class AccessoryValidationConstants
     {
-        public const int NameMaxLength = 30;
+        public const string NameIsRequiredError = "Accessory name is required!";
+        public const string NameMinLengthError = "Accessory name must be at least 3 symbols!";
+        public const string NameMaxLengthError = "Accessory name must no more than 50 symbols!";
+        public const string DescriptionIsRequiredError = "Accessory description is required!";
+        public const string DescriptionMinLengthError = "Accessory description must be at least 10 symbols!";
+        public const string DescriptionMaxLengthError = "Accessory description must no more than 100 symbols!";
+        public const string PriceIsRequiredError = "Accessory price is required!";
+
+        public const int NameMaxLength = 50;
+        public const int NameMinLength = 3;
         public const int DescriptionMaxLength = 100;
+        public const int DescriptionMinLength = 10;
+        public const int StockQuantityMinValue = 0;
+        public const int StockQuantityMaxValue = int.MaxValue;
+
+        public const string PriceMinValue = "0";
+        public const string PriceMaxValue = "79228162514264337593543950335";
     }
 }
