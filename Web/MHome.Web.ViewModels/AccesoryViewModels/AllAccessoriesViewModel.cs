57fd67418d8f8883619071fce2a46e8e01650683
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-08-31 10:30:21 +0300

    Add files via upload

---
+++ Web/MHome.Web.ViewModels/AccesoryViewModels/AllAccessoriesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MHome.Web.ViewModels.AccesoryViewModels
+{
+    public class AllAccessoriesViewModel
+    {
+        public ICollection<ListAllAccessoriesViewModel> AllAccessories { get; set; }
+
+        public string SearchQuery { get; set; }
+    }
+}
---
+++ Web/MHome.Web.ViewModels/AccesoryViewModels/ListAllAccessoriesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MHome.Data.Models;
+using MHome.Services.Mapping;
+
+namespace MHome.Web.ViewModels.AccesoryViewModels
+{
+    public class ListAllAccessoriesViewModel : IMapFrom<Accessory>
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Dimensions { get; set; }
+
+        public int StockQuantity { get; set; }
+    }
+}
