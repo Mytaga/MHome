1b6e2f6adeacff263f581e942a6b5a93f9e4145d
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-03 20:58:59 +0300

    Add files via upload

---
+++ Web/MHome.Web/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MHome.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
