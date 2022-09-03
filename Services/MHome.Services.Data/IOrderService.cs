b0b7fbb1837128b4fa634b79a52d7ff0e9f8b877
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-03 21:09:30 +0300

    Add files via upload

---
+++ Services/MHome.Services.Data/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using MHome.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MHome.Services.Data
+{
+    public interface IOrderService
+    {
+        ICollection<Order> All();
+
+        Task<Order> GetByIdАsync(string id);
+
+        Order GetById(string id);
+
+        Task AddOrder(Order order);
+
+        void DeleteOrder(Order order);
+    }
+}
---
+++ Services/MHome.Services.Data/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using MHome.Data.Common.Repositories;
+using MHome.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MHome.Services.Data
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IDeletableEntityRepository<Order> orderRepo;
+
+        public OrderService(IDeletableEntityRepository<Order> orderRepo)
+        {
+            this.orderRepo = orderRepo;
+        }
+
+        public Task AddOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Order> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetByIdАsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
