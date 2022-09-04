46cd0e08d6de2c92cc45595ff52648a408769f9d
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-04 22:15:11 +0300

    Add files via upload

---
+++ Web/MHome.Web.ViewModels/OrderViewModels/AllOrdersViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MHome.Web.ViewModels.OrderViewModels
+{
+    public class AllOrdersViewModel
+    {
+        public ICollection<ListAllOrdersViewModel> AllOrders { get; set; }
+
+        public string SearchQuery { get; set; }
+    }
+}
---
+++ Web/MHome.Web.ViewModels/OrderViewModels/ListAllOrdersViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MHome.Data.Models;
+using MHome.Data.Models.Enums;
+using MHome.Services.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHome.Web.ViewModels.OrderViewModels
+{
+    public class ListAllOrdersViewModel : IMapFrom<Order>
+    {
+        public string ClientName { get; set; }
+
+        public DateTime TimeOfOrder { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public DeliveryType DeliveryType { get; set; }
+    }
+}
