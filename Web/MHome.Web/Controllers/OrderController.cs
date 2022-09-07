7833dabce29aaecff0424d98ca15a881fa614573
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-07 14:07:51 +0300

    Add files via upload

--- Web/MHome.Web/Controllers/OrderController.cs
+++ Web/MHome.Web/Controllers/OrderController.cs
@@ -3,17 +3,24 @@ using MHome.Services.Data;
 using MHome.Services.Mapping;
 using MHome.Web.ViewModels.OrderViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace MHome.Web.Controllers
 {
     public class OrderController : Controller
     {
         private readonly IOrderService orderService;
+        private readonly IFurnitureService furnitureService;
+        private readonly IAccessoryService accessoryService;
 
-        public OrderController(IOrderService orderService)
+        public OrderController(IOrderService orderService, IFurnitureService furnitureService, IAccessoryService accessoryService)
         {
             this.orderService = orderService;
+            this.furnitureService = furnitureService;
+            this.accessoryService = accessoryService;
         }
 
         [HttpGet]
@@ -28,6 +35,36 @@ namespace MHome.Web.Controllers
             };
 
             return this.View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(string id, CreateOrderInputViewModel model)
+        {
+            if (this.furnitureService.ExistById(id))
+            {
+                var product = await this.furnitureService.GetByIdАsync(id);
+            }
+
+            if (this.accessoryService.ExistById(id))
+            {
+                var product = await this.accessoryService.GetByIdАsync(id);
+            }
+
+            var clientId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            model.ClientId = clientId;
+
+            Order order = AutoMapperConfig.MapperInstance.Map<Order>(model);
+
+            await this.orderService.AddOrder(order);
+
+            return this.RedirectToAction("Index", "Home");
         }
     }
 }
