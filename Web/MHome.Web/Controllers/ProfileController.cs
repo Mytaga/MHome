79d1baeb837bb9648aec8c93f5b5592b7eb53efb
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-14 17:05:42 +0300

    Add files via upload

---
+++ Web/MHome.Web/Controllers/ProfileController.cs
@@ -0,0 +1,20 @@
+﻿using MHome.Services.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MHome.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IClientService clientService;
+
+        public ProfileController(IClientService clientService)
+        {
+            this.clientService = clientService;
+        }
+
+        public IActionResult Main()
+        {
+            return this.View();
+        }
+    }
+}
