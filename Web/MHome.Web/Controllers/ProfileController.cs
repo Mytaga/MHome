b5bc7333677f1da9fe51eec5ac17575354aac459
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-15 13:59:17 +0300

    Add files via upload

--- Web/MHome.Web/Controllers/ProfileController.cs
+++ Web/MHome.Web/Controllers/ProfileController.cs
@@ -1,15 +1,33 @@
 ﻿using MHome.Services.Data;
+using MHome.Services.Mapping;
+using MHome.Web.ViewModels.ProfileViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace MHome.Web.Controllers
 {
     public class ProfileController : Controller
     {
         private readonly IClientService clientService;
+        private readonly IUserService userService;
 
-        public ProfileController(IClientService clientService)
+        public ProfileController(IClientService clientService, IUserService userService)
         {
             this.clientService = clientService;
+            this.userService = userService;
+        }
+
+        public IActionResult Main()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var user = this.userService.GetById(userId);
+
+            var clientProfile = user.Client;
+
+            ListProfileViewModel viewModel = AutoMapperConfig.MapperInstance.Map<ListProfileViewModel>(clientProfile);
+
+            return this.View(viewModel);
         }
 
         public IActionResult Create()
