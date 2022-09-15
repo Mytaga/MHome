0bbcad66acc42627617da0ebced3b3871de1038b
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-15 13:59:54 +0300

    Add files via upload

---
+++ Web/MHome.Web.ViewModels/ProfileViewModels/ListProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MHome.Data.Models;
+using MHome.Services.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHome.Web.ViewModels.ProfileViewModels
+{
+    public class ListProfileViewModel : IMapFrom<Client>
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
