06aeb8fd8e5fd19d054f925cb5ff56f018f38528
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-16 11:36:57 +0300

    Add files via upload

--- Web/MHome.Web.ViewModels/ProfileViewModels/EditProfileViewModel.cs
+++ Web/MHome.Web.ViewModels/ProfileViewModels/EditProfileViewModel.cs
@@ -1,19 +1,20 @@
 ﻿using MHome.Data.Models;
 using MHome.Services.Mapping;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MHome.Web.ViewModels.ProfileViewModels
 {
     public class EditProfileViewModel : IMapFrom<Client>
     {
+        public string Id { get; set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
         public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
