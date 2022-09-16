c0c31ca2469ac1665fa7a33a247be3fe83d8ac2b
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-16 14:33:31 +0300

    Add files via upload

--- MHome.Data.Models.Common/ClientValidationConstants.cs
+++ MHome.Data.Models.Common/ClientValidationConstants.cs
@@ -8,7 +8,16 @@ namespace MHome.Data.Models.Common
 {
     public static class ClientValidationConstants
     {
+        public const int FirstNameMinLength = 2;
         public const int FirstNameMaxLength = 30;
+        public const int LastNameMinLength = 2;
         public const int LastNameMaxLength = 30;
+
+        public const string FirstNameIsRequiredError = "Client first name is required!";
+        public const string LastNameIsRequiredError = "Client last name is required!";
+        public const string FirstNameMinLengthError = "Client first name must be at least 2 symbols!";
+        public const string FirstNameMaxLengthError = "Client first name must no more than 30 symbols!";
+        public const string LastNameMinLengthError = "Client last name must be at least 2 symbols!";
+        public const string LastNameMaxLengthError = "Client last name must no more than 30 symbols!";
     }
 }
