9f3d68bdedcb89efb0d5ba10004af45d01f6a69c
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-19 16:40:23 +0300

    Add files via upload

--- Web/MHome.Web.ViewModels/ProfileViewModels/ListProfileViewModel.cs
+++ Web/MHome.Web.ViewModels/ProfileViewModels/ListProfileViewModel.cs
@@ -1,14 +1,10 @@
-﻿using MHome.Data.Models;
+﻿using AutoMapper;
+using MHome.Data.Models;
 using MHome.Services.Mapping;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MHome.Web.ViewModels.ProfileViewModels
 {
-    public class ListProfileViewModel : IMapFrom<Client>
+    public class ListProfileViewModel : IMapFrom<Client>, IHaveCustomMappings
     {
         public string Id { get; set; }
 
@@ -18,6 +14,17 @@ namespace MHome.Web.ViewModels.ProfileViewModels
 
         public string PhoneNumber { get; set; }
 
+        public string Address { get; set; }
+
+        public string Town { get; set; }
+
         public string ImageUrl { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Client, ListProfileViewModel>()
+                .ForMember(d => d.Address, mo => mo.MapFrom(s => s.Address.AddressText))
+                .ForMember(d => d.Town, mo => mo.MapFrom(s => s.Address.TownName));
+        }
     }
 }
