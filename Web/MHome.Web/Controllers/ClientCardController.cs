c829d3b4d27c1c963e1791424f04d6d3fab60f51
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-20 11:22:52 +0300

    Add files via upload

---
+++ Web/MHome.Web/Controllers/ClientCardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MHome.Web.Controllers
+{
+    public class ClientCardController : Controller
+    {
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
