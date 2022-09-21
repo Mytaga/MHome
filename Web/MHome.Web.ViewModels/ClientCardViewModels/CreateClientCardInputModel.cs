2514549a4fe3d6511b59a26da487182a571bc93c
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-21 10:59:24 +0300

    Add files via upload

---
+++ Web/MHome.Web.ViewModels/ClientCardViewModels/CreateClientCardInputModel.cs
@@ -0,0 +1,22 @@
+﻿using MHome.Data.Models;
+using MHome.Services.Mapping;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MHome.Web.ViewModels.ClientCardViewModels
+{
+    public class CreateClientCardInputModel : IMapTo<ClientCard>
+    {
+        private readonly Random random = new Random();
+
+        [Required]
+        public string CardNumber => this.random.Next(1, 100000).ToString();
+
+        [Required]
+        public string ExpirationDate => DateTime.Now.AddYears(2).ToString();
+
+        public int? Discount => 10;
+
+        public string ClientId { get; set; }
+    }
+}
