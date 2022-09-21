fb6f37a09449daf4d516a1898a363ab07da66f3c
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-21 10:58:57 +0300

    Add files via upload

---
+++ Services/MHome.Services.Data/ClientCardService.cs
@@ -0,0 +1,34 @@
+﻿using MHome.Data.Common.Repositories;
+using MHome.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHome.Services.Data
+{
+    public class ClientCardService : IClientCardService
+    {
+        private readonly IDeletableEntityRepository<ClientCard> clientCardRepo;
+
+        public ClientCardService(IDeletableEntityRepository<ClientCard> clientCardRepo)
+        {
+            this.clientCardRepo = clientCardRepo;
+        }
+
+        public async Task AddClientCard(ClientCard clientCard)
+        {
+            await this.clientCardRepo.AddAsync(clientCard);
+            await this.clientCardRepo.SaveChangesAsync();
+        }
+
+        public ClientCard GetById(string id)
+        {
+            return this.clientCardRepo.All().FirstOrDefault(cc => cc.Id == id);
+        }
+
+        public async Task<ClientCard> GetByIdАsync(string id)
+        {
+            return await this.clientCardRepo.All().FirstOrDefaultAsync(cc => cc.Id == id);
+        }
+    }
+}
---
+++ Services/MHome.Services.Data/IClientCardService.cs
@@ -0,0 +1,14 @@
+﻿using MHome.Data.Models;
+using System.Threading.Tasks;
+
+namespace MHome.Services.Data
+{
+    public interface IClientCardService
+    {
+        Task<ClientCard> GetByIdАsync(string id);
+
+        ClientCard GetById(string id);
+
+        Task AddClientCard(ClientCard clientCard);
+    }
+}
