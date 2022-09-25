6338751b2782be22d7d8ee82a90e0bd59074f728
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-25 15:07:22 +0300

    Add files via upload

--- Web/MHome.Web.ViewModels/AccessoryViewModels/AccessoryDetailsViewModel.cs
+++ Web/MHome.Web.ViewModels/AccessoryViewModels/AccessoryDetailsViewModel.cs
@@ -11,6 +11,8 @@ namespace MHome.Web.ViewModels.AccessoryViewModels
 
         public decimal Price { get; set; }
 
+        public decimal PriceWithDiscount => this.Price * 0.90M;
+
         public string ImageURL { get; set; }
 
         public int StockQuantity { get; set; }
