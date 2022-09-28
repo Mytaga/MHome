c5899a9f8a257d26474c6b683966badfb7a0aec9
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-28 18:10:43 +0300

    Add files via upload

---
+++ Web/MHome.Web/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using MHome.Services.Data;
+using MHome.Services.Mapping;
+using MHome.Web.ViewModels.StoreViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace MHome.Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly IStoreService storeService;
+
+        public StoreController(IStoreService storeService)
+        {
+            this.storeService = storeService;
+        }
+
+        [HttpGet]
+        public IActionResult All()
+        {
+            var allStores = this.storeService.GetAll();
+
+            AllStoresViewModel viewModel = new AllStoresViewModel()
+            {
+                AllStores = allStores.To<ListAllStoresViewModel>().ToArray(),
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
