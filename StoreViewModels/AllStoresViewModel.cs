30fe5a607e7485e14fbfd1b2f6d15e921466bf8d
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-28 17:17:00 +0300

    Delete StoreViewModels directory

--- StoreViewModels/AllStoresViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MHome.Web.ViewModels.StoreViewModels
-{
-    public class AllStoresViewModel
-    {
-        public ICollection<ListAllStoresViewModel> AllStores { get; set; }
-    }
-}
