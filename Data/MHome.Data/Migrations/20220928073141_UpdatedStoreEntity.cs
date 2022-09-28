95f9f640c6f3b7d01be3b7a99ae846ecb025c9a6
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-28 10:33:07 +0300

    Add files via upload

---
+++ Data/MHome.Data/Migrations/20220928073141_UpdatedStoreEntity.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MHome.Data.Migrations
+{
+    public partial class UpdatedStoreEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Stores",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Stores",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Stores");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Stores");
+        }
+    }
+}
