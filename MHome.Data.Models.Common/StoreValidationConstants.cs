fa5e47b812a046812e19ab46e0b106a362c90f3a
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-09-29 15:46:58 +0300

    Add files via upload

--- MHome.Data.Models.Common/StoreValidationConstants.cs
+++ MHome.Data.Models.Common/StoreValidationConstants.cs
@@ -8,9 +8,24 @@ namespace MHome.Data.Models.Common
 {
     public static class StoreValidationConstants
     {
+        public const int NameMinLength = 3;
         public const int NameMaxLength = 30;
         public const int DescriptionMaxLength = 100;
         public const int EmailMaxLength = 50;
+        public const int PhoneNumberMinLength = 10;
         public const int PhoneNumberMaxLength = 20;
+
+        public const string NameIsRequiredError = "Store name is required!";
+        public const string DescriptionIsRequiredError = "Description is required!";
+        public const string EmailIsRequiredError = "Email is required!";
+        public const string PhoneNumberIsRequiredError = "Phone number is required!";
+        public const string AddressIsRequiredError = "Address is required!";
+        public const string TownNameIsRequiredError = "Town name is required!";
+        public const string EmailMaxLengthError = "Email must be no more than 50 symbols!";
+        public const string NameMinLengthError = "Store name must at least 3 symbols!";
+        public const string NameMaxLengthError = "Store name must no more than 30 symbols!";
+        public const string DescriptionMaxLengthError = "Description must no more than 100 symbols!";
+        public const string PhoneNumberMinLengthError = "Phone number must at least 10 symbols!";
+        public const string PhoneNumberMaxLengthError = "Phone number must no more than 20 symbols!";
     }
 }
