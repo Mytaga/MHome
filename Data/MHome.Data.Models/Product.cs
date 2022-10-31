c0b7d9be65b0670fcf19fe3bd513b0f9aeb18b43
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-31 17:22:41 +0200

    Add files via upload

---
+++ Data/MHome.Data.Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using PizzaOrderingSystem.Data.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static PizzaOrderingSystem.Common.ModelValidationConstants;
+
+namespace PizzaOrderingSystem.Data.Models
+{
+    public class Product : BaseDeletableModel<string>
+    {
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Orders = new HashSet<Order>();
+            this.Shops = new HashSet<Shop>();
+        }
+
+        [Required]
+        [MaxLength(ProductVadidation.NameMaxLength)]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(ProductVadidation.DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+
+        public virtual ICollection<Shop> Shops { get; set; }
+    }
+}
